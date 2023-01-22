1e2498807b0bc9eed7e34dbcabdcd9fc98c10400
Archit Gosain <architgosain0601@gmail.com>
2023-01-22 17:32:06 +0530

    Create application allows to specify the dimensions and title

--- src/Editor/Application.h
+++ src/Editor/Application.h
@@ -1,25 +1,31 @@
 #pragma once
 #include "common/Types.h"
 #include "Window.h"
+#include <iostream>
 
 class Application
 {
 private:
 	static Application* s_Instance;
-	int m_Width = 1920, m_Height = 1080;
-	const char* m_Title = "Newton";
+	int m_Width, m_Height;
+	const char* m_Title;
 	Application();
 	Application(int width, int height, const char* title);
+
+	Application* getInstanceImpl(int width, int height, char* title);
 	Ptr<Window> m_Window;
-	bool m_Running = true;
 
 public:
 	void Run();
-	static Application* GetInstance()
+	bool IsRunning();
+
+	// TODO: Fix this semantically
+	static Application* GetInstance(int width = 1920, int height = 1080, const char* title = "Newton")
 	{
-		if (s_Instance == nullptr)
-			s_Instance = new Application();
+		if (s_Instance == NULL)
+			s_Instance = new Application(width, height, title);
 		return s_Instance;
 	}
+
 	Window& GetWindow() { return *m_Window; }
 };
